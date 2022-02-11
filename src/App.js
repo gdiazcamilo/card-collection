ee88f8948772437e9e6433b6189d01a912e44b37
Gustavo Díaz <gdiazcamilo@gmail.com>
2022-02-11 14:48:42 -0400

    add card-list and card componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to see.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = { monsters: [] };
+  }
+
+  loadMonstersWithThenSyntax() {
+    console.log("loading with then syntax");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  loadMonstersWithAsyncSyntax = async () => {
+    console.log("loading with async syntax");
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+    this.setState({ monsters: users });
+  };
+
+  componentDidMount() {
+    this.loadMonstersWithThenSyntax();
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./card.styles.css";
+
+export const Card = (props) => {
+  return (
+    <div className='card-container'>
+      <img
+        alt={`monster${props.monster.id}`}
+        src={`https://robohash.org/${props.monster.id}?set=set2&size=280x280`}
+      />
+      <h2>{props.monster.name}</h2>
+      <p>{props.monster.email}</p>
+    </div>
+  );
+};
